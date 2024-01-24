37f78e277499234d275c393b4bc603c5bf52e3cb
PointBl_nk <125140770+Zeven100@users.noreply.github.com>
2024-01-24 21:48:37 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@
 const mongoose = require("mongoose")
 // Never push mongodb links on github !!!
 
-mongoose.connect("mongodb+srv://admin:IRi1BHNQ5Pn4A3YK@cluster0.mq6wgc5.mongodb.net/todos")
+mongoose.connect("your-mongodb-link")
 const todoSchema = mongoose.Schema({
     title :String ,
     description : String ,
